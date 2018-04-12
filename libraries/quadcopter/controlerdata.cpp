2f228b585e0e7cf257a06fbf5d7f3d91a368c5ab
Gabriel Gyori <gabrielgyori@yahoo.com>
2018-04-12 21:00:55 +0200

    Oprava buildu

--- libraries/quadcopter/controlerdata.cpp
+++ libraries/quadcopter/controlerdata.cpp
@@ -43,10 +43,10 @@ void ControlerData::readDirectionMessage(const Message &msg)
     m_directionData.left  = east.asInt();
     m_directionData.right = west.asInt();
 
-    TRACE_VAR("North: ", m_directionData.upper, DBG);
-    TRACE_VAR("South: ", m_directionData.lower, DBG);
-    TRACE_VAR("East: ",  m_directionData.left, DBG);
-    TRACE_VAR("West: ",  m_directionData.right, DBG);
+    TRACE_VAR("North: ", m_directionData.upper, INF);
+    TRACE_VAR("South: ", m_directionData.lower, INF);
+    TRACE_VAR("East: ",  m_directionData.left, INF);
+    TRACE_VAR("West: ",  m_directionData.right, INF);
 }
 
 void ControlerData::readThrottleMessage(const Message &msg)
@@ -63,8 +63,8 @@ void ControlerData::readThrottleMessage(const Message &msg)
     m_throttleData.left  = left.asInt();
     m_throttleData.right = right.asInt();
 
-    TRACE_VAR("Up:    ", m_throttleData.upper, DBG);
-    TRACE_VAR("Down:  ", m_throttleData.lower, DBG);
-    TRACE_VAR("Left:  ", m_throttleData.left,  DBG);
-    TRACE_VAR("Right: ", m_throttleData.right, DBG);
+    TRACE_VAR("Up:    ", m_throttleData.upper, INF);
+    TRACE_VAR("Down:  ", m_throttleData.lower, INF);
+    TRACE_VAR("Left:  ", m_throttleData.left,  INF);
+    TRACE_VAR("Right: ", m_throttleData.right, INF);
 }
